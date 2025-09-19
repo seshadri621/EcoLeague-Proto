d372446c17044f3059e1846eba89e009f1316f28
itsseshahere <itsseshahere@gmail.com>
2025-09-19 13:11:17 +0000

    fix: Removed ThemeProvider from index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -4,14 +4,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles/tailwind.css";
 import "./styles/index.css";
-import { ThemeProvider } from "./contexts/ThemeContext";
 import "./firebase"; // Import Firebase configuration
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
-  <ThemeProvider>
     <App />
-  </ThemeProvider>
 );
